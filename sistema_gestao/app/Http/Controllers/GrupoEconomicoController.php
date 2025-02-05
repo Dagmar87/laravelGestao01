be1d6f0f73b6afa4346059cb62a641859234ff67
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-05 11:49:00 -0300

    Desenvolvimento do codigo e inserção das funções no GrupoEconomicoController

--- sistema_gestao/app/Http/Controllers/GrupoEconomicoController.php
+++ sistema_gestao/app/Http/Controllers/GrupoEconomicoController.php
@@ -3,8 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\GrupoEconomico;
 
 class GrupoEconomicoController extends Controller
 {
-    //
+    public function index()
+    {
+        $grupo_economicos = GrupoEconomico::all();
+        return view("grupo_economicos.index", compact("grupo_economicos"));
+    }
+
+    public function create()
+    {
+        return view("grupo_economicos.create");
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            "nome" => "required|string|max:255",
+            "dataDeCriacao" => "required|date",
+            "ultimaAtualizacao" => "required|date",
+        ]);
+        GrupoEconomico::create($validatedData);
+        return redirect()
+            ->route("grupo_economicos.index")
+            ->with("success", "Grupo Economico criado com sucesso.");
+    }
+
+    public function edit(string $id)
+    {
+        $grupoEconomico = GrupoEconomico::findOrFail($id);
+        return view("grupo_economicos.edit", compact("grupoEconomico"));
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $grupoEconomico = GrupoEconomico::findOrFail($id);
+        $validatedData = $request->validate([
+            "nome" => "required|string|max:255",
+            "dataDeCriacao" => "required|date",
+            "ultimaAtualizacao" => "required|date",
+        ]);
+        $grupoEconomico->update($validatedData);
+        return redirect()
+            ->route("grupo_economicos.index")
+            ->with("success", "Grupo Economico atualizado com sucesso.");
+    }
+
+    public function destroy(string $id)
+    {
+        $grupoEconomico = GrupoEconomico::findOrFail($id);
+        $grupoEconomico->delete();
+        return redirect()
+            ->route("grupo_economicos.index")
+            ->with("success", "Grupo Economico excluido com sucesso.");
+    }
 }
