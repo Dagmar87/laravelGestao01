ca504f714549d1c166cc29bc1d901ea72b406408
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-05 12:03:01 -0300

    Ajuste no codigo das rotas do GrupoEconomico

--- sistema_gestao/routes/web.php
+++ sistema_gestao/routes/web.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\GrupoEconomicoController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('grupo_economicos', GrupoEconomicoController::class);
+Route::resource('/grupo_economicos', GrupoEconomicoController::class);
