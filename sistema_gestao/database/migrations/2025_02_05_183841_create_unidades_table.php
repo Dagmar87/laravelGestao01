fa6906ce2a5932b7a2466785591f3f33fe0e03ae
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-06 15:02:20 -0300

    Desenvolvimento do codigo e inserção dos parametros da tabela Unidade no migration

--- sistema_gestao/database/migrations/2025_02_05_183841_create_unidades_table.php
+++ sistema_gestao/database/migrations/2025_02_05_183841_create_unidades_table.php
@@ -4,16 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('unidades', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('nome_fantasia');
+            $table->string('razao_social');
+            $table->integer('cnpj');
+            $table->integer('bandeira_id')->unsigned()->nullable();
             $table->timestamps();
+        });
+
+        Schema::table('unidades', function ($table) {
+            $table->foreign('bandeira_id')->references('id')->on('bandeiras');
         });
     }
 
