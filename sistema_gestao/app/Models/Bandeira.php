ffbc12326dfd625200490d027c6c0c6e3a02d62a
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-06 15:08:45 -0300

    Desenvolvimento do codigo e inserção dos parametros da tabela Unidade na variavel fillable no model

--- sistema_gestao/app/Models/Bandeira.php
+++ sistema_gestao/app/Models/Bandeira.php
@@ -11,8 +11,5 @@ class Bandeira extends Model
     use HasFactory;
     protected $fillable = ['nome', 'grupo_economico_id'];
 
-    public function grupo_economico()
-    {
-        return $this->belongsTo(GrupoEconomico::class);
-    }
+    bandeira_id
 }
--- sistema_gestao/app/Models/Unidade.php
+++ sistema_gestao/app/Models/Unidade.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Unidade extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['nome_fantasia', 'razao_social', 'cnpj', 'bandeira_id'];
+
+    public function bandeira()
+    {
+        return $this->belongsTo(Bandeira::class);
+    }
 }
