d20fa14ae3e476612e150af8300edf3750ed76dd
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-06 10:55:34 -0300

    Adição das rotas da Bandeira

--- sistema_gestao/routes/web.php
+++ sistema_gestao/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\BandeiraController;
 use App\Http\Controllers\GrupoEconomicoController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('/grupo_economicos', GrupoEconomicoController::class);
+Route::resource('/bandeiras', BandeiraController::class);
