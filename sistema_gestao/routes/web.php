5df3755ca8fcca4e215c7d6dc6ed283ce4b6f2fd
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-06 15:53:09 -0300

    Adição das rotas da Unidade

--- sistema_gestao/routes/web.php
+++ sistema_gestao/routes/web.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\BandeiraController;
 use App\Http\Controllers\GrupoEconomicoController;
+use App\Http\Controllers\UnidadeController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('/grupo_economicos', GrupoEconomicoController::class);
 Route::resource('/bandeiras', BandeiraController::class);
+Route::resource('/unidades', UnidadeController::class);
