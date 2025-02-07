391df3dbfafc572219858401791faf0f98696a68
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-07 11:57:16 -0300

    Adição das rotas do Colaborador

--- sistema_gestao/routes/web.php
+++ sistema_gestao/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BandeiraController;
+use App\Http\Controllers\ColaboradorController;
 use App\Http\Controllers\GrupoEconomicoController;
 use App\Http\Controllers\UnidadeController;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +9,4 @@ use Illuminate\Support\Facades\Route;
 Route::resource('/grupo_economicos', GrupoEconomicoController::class);
 Route::resource('/bandeiras', BandeiraController::class);
 Route::resource('/unidades', UnidadeController::class);
+Route::resource('/colaboradors', ColaboradorController::class);
