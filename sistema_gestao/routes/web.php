52312b7df107c04046494ac2a1152a8caf9130f8
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-07 17:13:18 -0300

    Implentação da parte de Autenticação do projeto

--- sistema_gestao/routes/web.php
+++ sistema_gestao/routes/web.php
@@ -1,12 +1,20 @@
 <?php
 
-use App\Http\Controllers\BandeiraController;
-use App\Http\Controllers\ColaboradorController;
-use App\Http\Controllers\GrupoEconomicoController;
-use App\Http\Controllers\UnidadeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('/grupo_economicos', GrupoEconomicoController::class);
-Route::resource('/bandeiras', BandeiraController::class);
-Route::resource('/unidades', UnidadeController::class);
-Route::resource('/colaboradors', ColaboradorController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
