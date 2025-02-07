ea58a76df0465b58c538c4744a2af5c3b76e168f
Dagmar87 <jdfssobrinho@hotmail.com>
2025-02-07 11:42:15 -0300

    Desenvolvimento do codigo e inserção dos parametros da tabela Colaborador na variavel fillable no model

--- sistema_gestao/app/Models/Colaborador.php
+++ sistema_gestao/app/Models/Colaborador.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Colaborador extends Model
 {
-    //
+    use HasFactory;
+    
+    protected $fillable = ['nome','email','cpf','unidade_id'];
+
+    public function unidade()
+    {
+        return $this->belongsTo(Unidade::class);
+    }
 }
